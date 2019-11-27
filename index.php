bf0d4a29f811ad27fceef4091b62af1783d5e2a2
Natalia <nath.atlia@gmail.com>
2019-11-27 08:03:26 -0600

    index php

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/stylespilaresg.css">
+    <link href="https://fonts.googleapis.com/css?family=DM+Serif+Text|Open+Sans:300&display=swap" rel="stylesheet">
+    <title>Ecopoli</title>
+</head>
+<body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    
+    <div class="container">
+            <nav id="ecco">
+                    <h1 id="title">ECCOPOLI</h1>
+                    </nav>
+            <nav>
+                    <li><a href="#" class="line">Proyectos</a></li>
+                    <li><a href="#" class="line">Nosotros</a></li>
+                    <li><a href="#" class="line">Pilares</a></li>
+                    <li><a href="contacto.html" class="line">Contacto</a></li>
+                </nav>
+<div class="uno">     
+    <section >
+        <div class="parallax-text-wrap">
+            <span class="parallax-text">Uno</span>
+            <h3>Estado de Derecho y Derechos Humanos 
+                </h3>
+            
+        </div>
+    </section>
+        <div id="contuno">
+        <div class="unotxt">
+        <p>
+
+            El primer pilar y una de las metas más importantes de Eccopoli es el que se respete la ley y se mantenga el estado de derecho. A través de juntas comunitarias, Eccopoli crea sinergias para que se denuncien los crímenes, se persiga a los responsables y se haga cumplir la ley. 
+
+            Por otro lado, también a través de juntas comunitarias, con representantes del Ministerio de la Justicia, se promueve que los derechos humanos sean respetados en todas las índoles, también conforme a leyes y tratados internacionales. 
+
+        </p>
+    </div>
+    <div class="unoimg">
+            <img src="images/fff.png" alt="feliz">
+    </div>
+</div>
+</div>
+
+<div class="dos">
+       <section>
+        <div class="parallax-text-wrap"><span class="parallax-text">dos</span>
+            <h3>Equidad de Género 
+                </h3>
+        </div>
+    </section>
+
+        <div id="contdos">
+        <div class="unotxt">
+        <p>
+
+            Un segundo pilar de la ONG es el que prevalezca la equidad de género, de modo que en reuniones con comunidades alrededor de Haití, Eccopoli promueve que en todos los ámbitos, tanto en la familia, como en la política y en todos los ámbitos sociales, prevalezca esta importante equidad y se combata contra todo tipo de violencia contra las mujeres.
+        </p>
+    </div>
+    <div class="unoimg">
+        <img src="images/fff.png" alt="feliz">
+    </div>
+</div>
+</div>
+
+<div class="tres">
+        <section>
+                <div class="parallax-text-wrap"><span class="parallax-text">tres</span>
+                    <h3>Desarrollo Durable
+                        </h3>
+                </div>
+            </section>
+        
+                <div id="uno">
+                <div class="unotxt">
+                <p>
+        
+                    El tercer pilar de Eccopoli tiene que ver con el medio ambiente. Eccopoli tiene la intención de construir un nuevo proyecto cada año. La elección de cada proyecto se lleva a cabo en juntas comunitarias donde la población se hace expresar sobre sus necesidades. Eccopoli toma el marco de los diecisiete objetivos de desarrollo sustentable de las Naciones Unidas, para intencionalmente implementar un proyecto conforme a cada uno de esos diecisiete objetivos anualmente, además de los proyectos seleccionados con ayuda de la comunidad en cuestión 
+        
+                </p>
+            </div>
+            <div class="unoimg">
+                <img src="images/fff.png" alt="feliz">
+            </div>
+        </div>
+    </div>
+</div>
+    <script type="text/javascript" src="js/parallax.js"></script>
+</body>
+</html>
